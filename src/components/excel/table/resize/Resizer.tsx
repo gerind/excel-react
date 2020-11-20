5e095dcf01b14f62eeb1a83cfd940f6f139916d5
gerind <gerind@yandex.ru>
2020-11-20 18:05:05 +0300

    rewrite resize function

--- src/components/excel/table/resize/Resizer.tsx
+++ src/components/excel/table/resize/Resizer.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { resizeTable } from '../../../../core/redux/actions'
-import { addEventListeners, debounce, preventDefault, removeEventListeners } from '../../../../core/utils'
+import { mouseDownHandler } from './resize.handler'
 
 interface ResizerProps {
   type: 'row' | 'column'
@@ -13,79 +12,14 @@ const Resizer: React.FC<ResizerProps> = ({type, index}) => {
   const [styles, setStyles] = useState({})
   const dispatch = useDispatch()
 
-  const mouseDownHandler = type === 'column'
-      ? (downEvent: React.MouseEvent) => {
-          const windowHeight = window.innerHeight
-          setStyles({bottom: `-${windowHeight}px`, opacity: 1})
-          const prevX = downEvent.clientX
-          const mouseMoveHandler = debounce((moveEvent: MouseEvent) => {
-            const newX = moveEvent.clientX
-            setStyles(styles => ({
-              ...styles,
-              transform: `translate(${newX - prevX}px, 0)`
-            }))
-          }, 50)
-          const mouseUpHandler = (upEvent: MouseEvent) => {
-            const newX = upEvent.clientX
-            setStyles({})
-
-            dispatch(resizeTable({
-              side: 'column',
-              index,
-              delta: newX - prevX
-            }))
-
-            removeEventListeners(document, {
-              'selectstart': preventDefault,
-              'mousemove': mouseMoveHandler,
-              'mouseup': mouseUpHandler
-            })
-          }
-          addEventListeners(document, {
-            'selectstart': preventDefault,
-            'mousemove': mouseMoveHandler,
-            'mouseup': mouseUpHandler
-          })
-        }
-      : (downEvent: React.MouseEvent) => {
-        const windowWidth = window.innerWidth
-        setStyles({right: `-${windowWidth}px`, opacity: 1})
-        const prevY = downEvent.clientY
-        const mouseMoveHandler = debounce((moveEvent: MouseEvent) => {
-          const newY = moveEvent.clientY
-          setStyles(styles => ({
-            ...styles,
-            transform: `translate(0, ${newY - prevY}px)`
-          }))
-        }, 50)
-        const mouseUpHandler = (upEvent: MouseEvent) => {
-          const newY = upEvent.clientY
-          setStyles({})
-
-          dispatch(resizeTable({
-            side: 'row',
-            index,
-            delta: newY - prevY
-          }))
-
-          removeEventListeners(document, {
-            'selectstart': preventDefault,
-            'mousemove': mouseMoveHandler,
-            'mouseup': mouseUpHandler
-          })
-        }
-        addEventListeners(document, {
-          'selectstart': preventDefault,
-          'mousemove': mouseMoveHandler,
-          'mouseup': mouseUpHandler
-        })
-      }
+  const handler = useMemo(() =>
+    mouseDownHandler(type, index, setStyles, dispatch), [type, index])
 
   return (
     <div
       className={className}
       style={styles}
-      onMouseDown={mouseDownHandler}
+      onMouseDown={handler}
     />
   )
 }
---
+++ src/components/excel/table/resize/resize.handler.tsx
@@ -0,0 +1,42 @@
+import { resizeTable } from '../../../../core/redux/actions'
+import { addEventListeners, debounce, preventDefault, removeEventListeners } from '../../../../core/utils'
+
+export function mouseDownHandler(type: 'row' | 'column', index: number, setStyles: Function, dispatch: Function) {
+  return (downEvent: React.MouseEvent) => {
+    const isColumn = type === 'column'
+    const windowSize = window[isColumn ? 'innerHeight' : 'innerWidth']
+    const coordName = isColumn ? 'clientX' : 'clientY'
+    setStyles({[isColumn ? 'bottom' : 'right']: `-${windowSize}px`, opacity: 1})
+    const prevCoord = downEvent[coordName]
+    const mouseMoveHandler = debounce((moveEvent: MouseEvent) => {
+      const newCoord = moveEvent[coordName]
+      setStyles(styles => ({
+        ...styles,
+        transform: isColumn
+            ? `translate(${newCoord - prevCoord}px, 0)`
+            : `translate(0, ${newCoord - prevCoord}px)`
+      }))
+    }, 42)
+    const mouseUpHandler = (upEvent: MouseEvent) => {
+      const newCoord = upEvent[coordName]
+      setStyles({})
+
+      dispatch(resizeTable({
+        side: type,
+        index,
+        delta: newCoord - prevCoord
+      }))
+
+      removeEventListeners(document, {
+        'selectstart': preventDefault,
+        'mousemove': mouseMoveHandler,
+        'mouseup': mouseUpHandler
+      })
+    }
+    addEventListeners(document, {
+      'selectstart': preventDefault,
+      'mousemove': mouseMoveHandler,
+      'mouseup': mouseUpHandler
+    })
+  }
+}
