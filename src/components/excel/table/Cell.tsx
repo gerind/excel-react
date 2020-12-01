4fefa6bc2768e3eac778c2302363cd9aa2e003b6
gerind <gerind@yandex.ru>
2020-12-01 19:58:31 +0300

    remove comment from Cell

--- src/components/excel/table/Cell.tsx
+++ src/components/excel/table/Cell.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useContext, useEffect, useMemo, useRef, useState } from 'react'
+import React from 'react'
 import emitter from '../../../core/emitter'
 import { selectCell } from '../../../core/redux/actions'
 import { styleObject } from '../../../core/scriptTypes'
@@ -10,76 +10,6 @@ interface CellProps {
   rowIndex: number
   colIndex: number
 }
-/*
-const Cell: React.FC<CellProps> = ({rowIndex, colIndex}) => {
-
-  const [currentText, changeCurrentText] = useState('')
-  const [styleState, changeStyleState] = useState({})
-
-  const table = useContext(TableContext)
-  const [selected, setSelected] = useState(table.initial.nowSelected === cellToId(rowIndex, colIndex) ? 'selected' : '')
-  const [width, changeWidth] = useState<number>(table.initial.columnResize[colIndex])
-
-  const thisCellRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    table.cellsRef[rowIndex] = table.cellsRef[rowIndex] ?? {}
-    Object.assign(
-      table.cellsRef[rowIndex][colIndex] = table.cellsRef[rowIndex][colIndex] ?? {},
-      {
-        select() {
-          thisCellRef.current.focus()
-          setSelected('selected')
-        },
-        unselect() { setSelected('') },
-        target: thisCellRef.current,
-        change: changeCurrentText,
-        changeStyle(styles) {
-          changeStyleState(state => ({...state, ...styles}))
-        },
-        changeWidth
-      }
-    )
-  }, [])
-
-  const handler = useMemo(() =>
-      keyboardSelectionHandler(rowIndex, colIndex, table.dispatch), [rowIndex, colIndex, table.dispatch])
-
-  const styleObject = useMemo(() => ({
-    ...styleState,
-    width: width + 'px'
-  }), [styleState, width])
-
-  return (
-    <div
-      contentEditable={true}
-      suppressContentEditableWarning={true}
-      ref={thisCellRef}
-      className={`cell ${selected}`}
-      style={styleObject}
-      onMouseDown={() => {
-        table.dispatch(selectCell({
-          id: cellToId(rowIndex, colIndex)
-        }))
-      }}
-      onKeyDown={handler}
-      onInput={e => {
-        emitter.emit('table:input', { target: e.target })
-      }}
-      onBlur={e => {
-        changeCurrentText(getInnerText(e.target))
-      }}
-      onFocus={() => {
-        replaceCaret(thisCellRef.current)
-      }}
-    >
-      {currentText}
-    </div>
-  )
-}
-
-export default memo(Cell)
-*/
 
 class Cell extends React.PureComponent<CellProps> {
 
